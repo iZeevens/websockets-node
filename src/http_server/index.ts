805792bba918c1ced1a200320887d4b77d6cae54
iZeevens <izeevens@gmail.com>
2024-10-23 21:12:58 +0500

    fix: problem with running code

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,11 +2,12 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as http from 'http'
 
-export const httpServer = http.createServer(function (req, res) {
+export const httpServer = http.createServer((req, res) => {
   const __dirname = path.resolve(path.dirname(''))
-  const file_path =
+  const filePath =
     __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url)
-  fs.readFile(file_path, function (err, data) {
+
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(404)
       res.end(JSON.stringify(err))
